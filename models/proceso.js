53f0252798b4e5bcdc398d0773e71736131175cd
EdwinDG <halg830@gmail.com>
2024-04-04 14:44:34 -0500

    ndfs

--- models/proceso.js
+++ models/proceso.js
@@ -5,7 +5,7 @@ const Proceso = new mongoose.Schema(
         codigo: {type: String, require:true},
         presupuesto: {type: Number, require:true},
         presupuestoActual: {type: Number, require:true},
-        fecha: {type: Date, require:true},
+        fecha: {type: String, require:true},
         createAd: {type:Date, default:Date.now},
         estado : { type: Boolean, default:1},
     }
