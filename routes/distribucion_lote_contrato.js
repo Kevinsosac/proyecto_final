0cc295db72d11c093aa6bb1e6f304ef5daa68117
jhonbry <SENA@SGLDFPCATSD283.FormacionSena.RED>
2024-04-11 13:47:22 -0500

    dfjs

--- routes/distribucion_lote_contrato.js
+++ routes/distribucion_lote_contrato.js
@@ -8,15 +8,15 @@ const router=new Router()
 router.get('/dislote_contrato', httpdislote_contrato.getdislote_contrato)
 router.get('/dislote_contrato/:id', httpdislote_contrato.getdislote_contratoid)
 router.post('/agregar',[
-    check("id_contrato", "el telefono es obligatorio").not().isEmpty(),
-    check("id_lote", "el telefono es obligatorio").not().isEmpty(),
     check("presupuesto", "el telefono es obligatorio").not().isEmpty(),
+    check("iddiscontratolote", "el iddiscontratolote es obligatorio").not().isEmpty(),
+    check("iddisdependencia", "el iddisdependencia es obligatorio").not().isEmpty(),
     validarCampos
 ],httpdislote_contrato.postAgregardislote_contrato );
 router.put('/dislote_contrato/:id',[
-    check("id_contrato", "el telefono es obligatorio").not().isEmpty(),
-    check("id_lote", "el telefono es obligatorio").not().isEmpty(),
     check("presupuesto", "el telefono es obligatorio").not().isEmpty(),
+    check("iddiscontratolote", "el iddiscontratolote es obligatorio").not().isEmpty(),
+    check("iddisdependencia", "el iddisdependencia es obligatorio").not().isEmpty(),
 ], httpdislote_contrato.putEditardislote_contrato);
 router.put('/dislote_contrato/activar/:id', httpdislote_contrato.putdislote_contratoInactivar)
 router.put('/dislote_contrato/inactivar/:id', httpdislote_contrato.putdislote_contratoActivar)
