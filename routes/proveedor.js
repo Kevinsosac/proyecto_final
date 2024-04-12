778779416b1dec09d8b088fd75eb1596cfe9f37b
EdwinDG <halg830@gmail.com>
2024-04-12 14:10:38 -0500

    morir

--- routes/proveedor.js
+++ routes/proveedor.js
@@ -5,12 +5,12 @@ import validarCampos from "../middelwares/validator.js";
 
 const router=new Router()
 
-router.get('/producto', httpproveedor.getproveedor)
-router.get('/producto/:cedula',[
+router.get('/proveedor', httpproveedor.getproveedor)
+router.get('/proveedor/:cedula',[
     check("cedula", "El codigo es obligatorio").not().isEmpty(),
     validarCampos
 ], httpproveedor.getproveedorid)
-router.get('/producto/:nombre',[
+router.get('/proveedor/:nombre',[
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     validarCampos
 ], httpproveedor.getproveedornombre)
@@ -23,7 +23,7 @@ router.post('/agregar',[
     check("empresa", "El empresa unitario es obligatorio").not().isEmpty(),
     validarCampos
 ],httpproveedor.postAgregarproveedor );
-router.put('/producto/:id',[
+router.put('/proveedor/:id',[
     check("apellido", "el apellido es obligatorio").not().isEmpty(),
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("cedula", "La cedula es obligatoria").not().isEmpty(),
