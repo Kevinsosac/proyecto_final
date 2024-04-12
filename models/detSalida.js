f5f7c4b7f2b82b40c557cdae8039e8335a9d95c5
EdwinDG <halg830@gmail.com>
2024-04-12 16:40:58 -0500

    odoapisd

--- models/detSalida.js
+++ models/detSalida.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const Detsalida = new mongoose.Schema({
-    cantidadentregada: {type:Date, require:true },
+    cantidadentregada: {type:Number, require:true },
     idsalida: {type:mongoose.Schema.Types.ObjectId,ref:'Salida', require:true},
     idproducto: {type:mongoose.Schema.Types.ObjectId,ref:'Producto', require:true},
-    cantidadpendeinte: {type:Number},
+    cantidadpendeinte: {type:Number, require:true},
     estado:{type:Boolean, default:0},
     createAT : {type:Date,default: Date.now }
 });
