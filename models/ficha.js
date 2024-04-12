dc131a64561ce4ed59b0593c88eed16c34d41431
EdwinDG <halg830@gmail.com>
2024-04-12 15:39:32 -0500

    ficha

--- models/ficha.js
+++ models/ficha.js
@@ -8,7 +8,6 @@ const ficha = new Schema({
   nivel_de_formacion: { type: String, require: true },
   fecha_inicio: { type: String, require: true },
   ficha_fin: { type: String, require: true },
-  id_area: { type: mongoose.Schema.Types.ObjectId, ref: "Area", require: true },
   estado: { type: Boolean, default: 1 },
   createAT: { type: Date, default: Date.now },
 });
--- routes/ficha.js
+++ routes/ficha.js
@@ -24,7 +24,6 @@ router.post(
     check("nivel_de_formacion", "Digite el nivel").not().isEmpty(),
     check("fecha_inicio", "fecha").not().isEmpty(),
     check("ficha_fin", "fecha").not().isEmpty(),
-    check("id_area", "El nombre de ficha obligatorio").notEmpty(),
     validarCampos 
   ],
   httpFicha.postFicha
