3b2ff31482050997734e746668019e88a1269890
EdwinDG <halg830@gmail.com>
2024-04-12 15:55:47 -0500

    dfsdf

--- routes/entrada.js
+++ routes/entrada.js
@@ -8,10 +8,7 @@ const router = new Router()
 
 router.get("/all", httEntrada.getAllEntrada)
 router.get("/detalle", httEntrada.getdetalleEntrada)
-router.post("/agregar",[
-    check().notEmpty,
-    validarCampos
-], httEntrada.postAgregarEntrada)
+router.post("/agregar", httEntrada.postAgregarEntrada)
 router.put("/inactivar/:id",httEntrada.putEntrada_Inactivar); 
 router.put("/activar/:id", httEntrada.putEntrada_Activar); 
 
